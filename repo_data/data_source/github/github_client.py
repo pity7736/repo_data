579bd9cc724b3e9e4ee0badb6a528e6d3b487986
Julián Cortés <pity7736@gmail.com>
2020-10-17 23:31:55 -0500

    feat: added get_repository to GithubClient

--- repo_data/data_source/github/github_client.py
+++ repo_data/data_source/github/github_client.py
@@ -20,3 +20,19 @@ class GithubClient:
         if response.status_code == 200:
             return response.json()
         return {}
+
+    async def get_repository(self, owner, name):
+        token = os.environ['GITHUB_TOKEN']
+        async with httpx.AsyncClient() as client:
+            try:
+                response = await client.get(
+                    f'https://api.github.com/repos/{owner}/{name}',
+                    headers={
+                        'Authorization': f'token {token}'
+                    }
+                )
+            except httpx.RequestError:
+                return {}
+        if response.status_code == 200:
+            return response.json()
+        return {}
--- tests/integration_tests/test_github_client.py
+++ tests/integration_tests/test_github_client.py
@@ -1,11 +1,15 @@
-from pytest import mark
+from pytest import mark, fixture
 
 from repo_data.data_source.github.github_client import GithubClient
 
 
+@fixture
+def client():
+    return GithubClient()
+
+
 @mark.asyncio
-async def test_get_user():
-    client = GithubClient()
+async def test_get_user(client):
     user_data = await client.get_user('pity7736')
 
     assert user_data['login'] == 'pity7736'
@@ -13,8 +17,23 @@ async def test_get_user():
 
 
 @mark.asyncio
-async def test_get_non_existent_user():
-    client = GithubClient()
-    user_data = await client.get_user('qwertyzxc123')
+async def test_get_non_existent_user(client):
+    user_data = await client.get_user(username='qwertyzxc123')
+    assert user_data == {}
 
+
+@mark.asyncio
+async def test_get_repo(client):
+    repo_data = await client.get_repository(owner='pity7736', name='nyoibo')
+
+    assert repo_data['name'] == 'nyoibo'
+    assert repo_data['private'] is False
+    assert repo_data['description'] == 'Create automatically attribute accessor in ' \
+                                       'Python.'
+    assert repo_data['full_name'] == 'pity7736/nyoibo'
+
+
+@mark.asyncio
+async def test_get_non_existent_repo(client):
+    user_data = await client.get_repository(owner='pitty7736', name='qwerty')
     assert user_data == {}
