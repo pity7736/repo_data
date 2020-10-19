97ab6fe8f05a36191a5890a1dda6e7239994b54c
Julián Cortés <pity7736@gmail.com>
2020-10-18 23:23:03 -0500

    feat: run app locally

--- repo_data/__init__.py
+++ repo_data/__init__.py
@@ -1,25 +1,2 @@
-import os
-
-from tortoise import Tortoise
-
-
-async def init_db():
-    await Tortoise.init({
-        'connections': {
-            'default': {
-                'engine': 'tortoise.backends.asyncpg',
-                'credentials': {
-                    'host': os.environ['REPO_DATA_HOST'],
-                    'port': os.environ['REPO_DATA_PORT'],
-                    'user': os.environ['REPO_DATA_USER'],
-                    'password': os.environ['REPO_DATA_PASSWORD'],
-                    'database': os.environ['REPO_DATA_DATABASE'],
-                }
-            }
-        },
-        'apps': {
-            'models': {
-                'models': ['repo_data.models']
-            }
-        }
-    })
+from .api import app  # noqa: F401
+from .init_db import init_db  # noqa: F401
--- repo_data/api/main.py
+++ repo_data/api/main.py
@@ -1,7 +1,9 @@
+from dotenv import load_dotenv
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse
 from starlette.routing import Route, Mount
 
+from repo_data.init_db import init_db
 from repo_data.api.views import api_routes
 
 
@@ -9,10 +11,16 @@ async def hello_world(request):
     return JSONResponse({'hello': 'world'})
 
 
+async def _initializer():
+    load_dotenv()
+    await init_db()
+
+
 app = Starlette(
     debug=True,
     routes=[
         Route('/', hello_world),
         Mount('/api', routes=api_routes)
-    ]
+    ],
+    on_startup=[_initializer]
 )
