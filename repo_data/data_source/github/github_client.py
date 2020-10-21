cbcca33776d87267642db233739e3f733f109886
Julián Cortés <pity7736@gmail.com>
2020-10-21 00:26:01 -0500

    refactor: github client

--- repo_data/data_source/github/github_client.py
+++ repo_data/data_source/github/github_client.py
@@ -4,35 +4,39 @@ from repo_data import settings
 
 
 class GithubClient:
+    _token = settings.GITHUB_TOKEN
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls._client = httpx.AsyncClient()
+        return cls._instance
 
     async def get_user(self, username: str):
-        token = settings.GITHUB_TOKEN
-        async with httpx.AsyncClient() as client:
-            try:
-                response = await client.get(
-                    f'https://api.github.com/users/{username}',
-                    headers={
-                        'Authorization': f'token {token}'
-                    }
-                )
-            except httpx.RequestError:
-                return {}
+        try:
+            response = await self._client.get(
+                f'https://api.github.com/users/{username}',
+                headers={
+                    'Authorization': f'token {self._token}'
+                }
+            )
+        except httpx.RequestError:
+            return {}
         if response.status_code == 200:
             return response.json()
         return {}
 
     async def get_repository(self, owner, name):
-        token = settings.GITHUB_TOKEN
-        async with httpx.AsyncClient() as client:
-            try:
-                response = await client.get(
-                    f'https://api.github.com/repos/{owner}/{name}',
-                    headers={
-                        'Authorization': f'token {token}'
-                    }
-                )
-            except httpx.RequestError:
-                return {}
+        try:
+            response = await self._client.get(
+                f'https://api.github.com/repos/{owner}/{name}',
+                headers={
+                    'Authorization': f'token {self._token}'
+                }
+            )
+        except httpx.RequestError:
+            return {}
         if response.status_code == 200:
             return response.json()
         return {}
